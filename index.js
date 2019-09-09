a9ab1fc3a371eeb4580b9249309d3c53a8ddb479
marc13004 <marc.naouache@gmail.com>
2019-09-09 20:37:26 +0200

    adding upload

--- index.js
+++ index.js
@@ -3,21 +3,52 @@ const getResults = require('./scraper.js');
 
 //async function waiting the arrays of Objects chain
 (async () => {
+  try {
   let chain = await getResults();
+
+  let title;
+  let uri;
+  let author;
+  let points;
+  let comments;
+  let ranks;
+
   var news = { hackernews: [] };
 
-
 //looping to build the Json Object necessary prior printing it via a command
-  for (var i = 0; i < 30; i++) {
+// looping and handling error such has undefined value with a try catch statement
 
-    news.hackernews.push({
-        "title" : chain.chain[i].obj[0].children[4].children[0].children[0].data,
-        "uri"  : chain.chain[i].obj[0].children[4].children[0].attribs['href'],
-        "author"  : chain.chain[i].obj[0].next.children[1].children[3].children[0].data,
-        "points"  : chain.chain[i].obj[0].next.children[1].children[1].children[0].data,
-        "comments"  : chain.chain[i].obj[0].next.children[1].children[11].children[0].data,
-        "ranks"  : chain.chain[i].obj[0].children[1].children[0].children[0].data,
-    });
+  for (var i = 0; i < 30; i++) {
+    
+    title = null;
+    uri = null;
+    author = null;
+    points = null;
+    comments = null;
+    ranks = null;
+
+    try {
+      title = chain.chain[i].obj[0].children[4].children[0].children[0].data;
+      uri = chain.chain[i].obj[0].children[4].children[0].attribs['href'];
+      author = chain.chain[i].obj[0].next.children[1].children[3].children[0].data;
+      points = chain.chain[i].obj[0].next.children[1].children[1].children[0].data;
+      comments = chain.chain[i].obj[0].next.children[1].children[11].children[0].data;
+      ranks = chain.chain[i].obj[0].children[1].children[0].children[0].data;
+
+      news.hackernews.push({
+          "title" : title,
+          "uri"  : uri,
+          "author"  : author,
+          "points"  : points,
+          "comments"  : comments,
+          "ranks"  : ranks,
+      });
+
+    } catch(e) {
+      console.log(e.message);
+    } finally{
+    continue
+  }
 
   }
   //function to print n number of posts
@@ -36,5 +67,7 @@ const getResults = require('./scraper.js');
       .parse(process.argv);
 
 //      if (program.posts !== undefined) console.log(`integer: ${program.posts}`);
-
+} catch (e) {
+    console.log(e.message);
+  }
 })()
